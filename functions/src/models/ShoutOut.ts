4216166d7c11fe6c1dae802529ee08d6f8b76f62
Duy Pham <dvpham89@yahoo.com>
2022-06-09 16:36:29 -0400

    cicd

--- functions/src/models/ShoutOut.ts
+++ functions/src/models/ShoutOut.ts
@@ -1,8 +1,16 @@
 import { ObjectId } from "mongodb";
 
+export interface User {
+  displayName: string;
+  uid: string;
+}
+
 export default interface ShoutOut {
   _id?: ObjectId;
   to: string;
   from: string;
   text: string;
+  photoURL?: string;
+  image?: string;
+  likes?: User[];
 }
